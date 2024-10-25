18c500504a0df1f0211ae72b8ade5314d95263eb
lamanganjali <154550874+lamanganjali@users.noreply.github.com>
2024-10-25 15:43:41 +0400

    salam

---
+++ src/main/kotlin/com/example/projectt/ProjecttApplication.kt
@@ -0,0 +1,11 @@
+package com.example.projectt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjecttApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjecttApplication>(*args)
+}
